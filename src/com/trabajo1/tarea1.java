fd4511e895ce89ccc33930a1ff0a5d686f5164bc
JuanSanchez29 <tjuan@outlook.com.ar>
2022-11-09 14:39:48 -0300

    Primera vez subiendo algo en GitHub

---
+++ src/com/trabajo1/tarea1.java
@@ -0,0 +1,55 @@
+package com.trabajo1;
+
+public class tarea1 {
+
+    public static void main(String[] args) {
+// Metodo main creado!!
+
+    // Datos numericos enteros
+
+        byte diasDeLaSemana = 7;
+        System.out.println(diasDeLaSemana);
+
+        short edad = 23;
+        System.out.println(edad);
+
+        int anoDeNacimiento = 1999;
+        System.out.println(anoDeNacimiento);
+
+        long numeroTelefonico = 1126355809;
+        System.out.println(numeroTelefonico);
+
+    // Datos numericos decimales
+
+        float variable1 = 1.5f;
+        System.out.println(variable1);
+
+        double variable2 = 10.10d;
+        System.out.println(variable2);
+
+    // booleanos
+
+        boolean variable3 = false;
+        System.out.println(variable3);
+
+        boolean variable4 = true;
+        System.out.println(variable4);
+
+    // Texto
+
+        char letra = 'c';
+        System.out.println(letra);
+
+        String texto = "Este es un texto de prueba";
+        System.out.println(texto);
+
+
+
+    }
+
+    }
+
+
+
+
+
